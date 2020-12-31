c860c41dc98ed1cb301656e31c67623c55eb997d
Aleks Margarian <aleks.margarian1@gmail.com>
2020-12-31 01:00:54 -0800

    Improve AsyncFF (uses std::invoke now)

--- src/modules/AsyncFF.h
+++ src/modules/AsyncFF.h
@@ -19,18 +19,20 @@ namespace EGL3::Modules {
 
         template<class FunctorType, class... FunctorArgs>
         void Enqueue(FunctorType&& Functor, FunctorArgs&&... Args) {
-            Enqueue(std::async(std::launch::async, std::move(Functor), std::move(Args)...));
+            Enqueue(std::async(std::launch::async, [](FunctorType&& Functor, FunctorArgs&&... Args) {
+                std::invoke(std::move(Functor), std::move(Args)...);
+            }, std::move(Functor), std::move(Args)...));
         }
 
         template<class FunctorType, class CallbackType, class... FunctorArgs>
         void EnqueueCallback(FunctorType&& Functor, CallbackType&& Callback, FunctorArgs&&... Args) {
             Enqueue(std::async(std::launch::async, [](FunctorType&& Functor, CallbackType&& Callback, FunctorArgs&&... Args) {
                 if constexpr (std::is_void_v<std::invoke_result_t<FunctorType, FunctorArgs...>>) {
-                    Functor(std::move(Args)...);
+                    std::invoke(std::move(Functor), std::move(Args)...);
                     Callback();
                 }
                 else {
-                    Callback(Functor(std::move(Args)...));
+                    Callback(std::invoke(std::move(Functor), std::move(Args)...));
                 }
             }, std::move(Functor), std::move(Callback), std::move(Args)...));
         }
