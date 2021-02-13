c0428c8176ea229500cbccfe3481297bdf9911b0
Aleks Margarian <aleks.margarian1@gmail.com>
2021-02-13 13:46:38 -0800

    Add setters to archive header, add update info

--- src/storage/game/Header.h
+++ src/storage/game/Header.h
@@ -2,6 +2,7 @@
 
 #include "ArchiveVersion.h"
 #include "GameId.h"
+#include "UpdateInfo.h"
 
 #include <string>
 
@@ -39,20 +40,48 @@ namespace EGL3::Storage::Game {
             return std::string(Game, strnlen_s(Game, sizeof(Game)));
         }
 
+        void SetGame(const std::string& NewGame) {
+            strncpy_s(Game, NewGame.c_str(), NewGame.size());
+        }
+
         std::string GetVersionLong() const {
             return std::string(VersionStringLong, strnlen_s(VersionStringLong, sizeof(VersionStringLong)));
+        }
+
+        void SetVersionLong(const std::string& NewVersionLong) {
+            strncpy_s(VersionStringLong, NewVersionLong.c_str(), NewVersionLong.size());
         }
 
         std::string GetVersionHR() const {
             return std::string(VersionStringHR, strnlen_s(VersionStringHR, sizeof(VersionStringHR)));
         }
 
+        void SetVersionHR(const std::string& NewVersionHR) {
+            strncpy_s(VersionStringHR, NewVersionHR.c_str(), NewVersionHR.size());
+        }
+
         GameId GetGameId() const {
             return GameNumeric;
         }
 
+        void SetGameId(GameId NewGameId) {
+            GameNumeric = NewGameId;
+        }
+
         uint64_t GetVersionNum() const {
             return VersionNumeric;
+        }
+
+        void SetVersionNum(uint64_t NewVersionNum) {
+            VersionNumeric = NewVersionNum;
+        }
+
+        const UpdateInfo& GetUpdateInfo() const {
+            return UpdateInfo;
+        }
+
+        UpdateInfo& GetUpdateInfo() {
+            return UpdateInfo;
         }
 
         static constexpr uint64_t GetOffsetManifestData() {
@@ -100,6 +129,7 @@ namespace EGL3::Storage::Game {
         char VersionStringHR[20];           // Human readable version - example: 13.30
         uint64_t VersionNumeric;            // Game specific, but a new update will always have a higher number (e.g. Fortnite's P4 CL)
         GameId GameNumeric;                 // Should be some sort of enum (GameId)
+        UpdateInfo UpdateInfo;              // If the archive is currently undergoing an update, this data should be valid
 
         static constexpr uint32_t ExpectedMagic = 0x41494745;
 
@@ -117,5 +147,7 @@ namespace EGL3::Storage::Game {
         static constexpr uint64_t FileDataSectorOffset = FileDataOffset / SectorSize;
     };
 #pragma pack(pop)
-    static_assert(sizeof(Header) == 144);
+    static_assert(sizeof(Header) == 174);
+
+    static_assert(sizeof(Header) < 256); // Hard stop, don't pass this under any circumstance
 }
---
+++ src/storage/game/UpdateInfo.h
@@ -0,0 +1,25 @@
+#pragma once
+
+namespace EGL3::Storage::Game {
+#pragma pack(push, 2)
+    struct UpdateInfo {
+        bool IsUpdating;                // Whether this data is valid and the archive is/was undergoing an unfinished update
+        uint64_t TargetVersion;         // The "VersionNumeric" that this info was updating to. Throw out this data if a newer update is available
+        uint64_t NanosecondsElapsed;    // Nanoseconds that have already been spent on updating
+        uint32_t PiecesComplete;        // Total "pieces" already downloaded and installed
+        uint64_t BytesDownloadTotal;    // Total bytes that were already downloaded
+
+        UpdateInfo() :
+            IsUpdating(false),
+            TargetVersion(-1),
+            NanosecondsElapsed(0),
+            PiecesComplete(0),
+            BytesDownloadTotal(0)
+        {
+
+        }
+    };
+#pragma pack(pop)
+
+    static_assert(sizeof(UpdateInfo) == 30);
+}
