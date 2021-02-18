95601ccaa43c7790ddf62ca64f03a5b1a99c0861
Aleks Margarian <aleks.margarian1@gmail.com>
2021-02-18 08:49:40 -0800

    Fix wrong alignment bug
    Never gonna reach that point (anytime soon), but just in case

--- src/utils/align.h
+++ src/utils/align.h
@@ -1,7 +1,7 @@
 #pragma once
 
 namespace EGL3::Utils {
-    template<int Alignment, class N>
+    template<uint64_t Alignment, class N>
     static constexpr N Align(N Value) {
         return (N)(((uint64_t)Value + Alignment - 1) & ~(Alignment - 1));
     }
