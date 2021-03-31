37c3a98a0cce97ab20c9e54971895606d1f3e284
Aleks Margarian <aleks.margarian1@gmail.com>
2021-03-31 11:18:33 -0700

    Update to 3.0.4 officially

--- src/web/xmpp/XmppClient.h
+++ src/web/xmpp/XmppClient.h
@@ -14,6 +14,10 @@
 namespace EGL3::Web::Xmpp {
     // Standards used are listed in comments inside the cpp file and in asserts
     // TODO: Create constant to easily switch environments (currently only in prod)
+    // TODO: HANDLE PARTY REQESTS
+    // <message id="F3BA65454F1533372F6FCBA8F03DD65D" from="d4d0142cc75141ff841b1b41b40bed3a@prod.ol.epicgames.com/V2:Fortnite:WIN::FA7BAD364934BD9B8CF445BD31D81622" xmlns="jabber:client" to="9baf4e2cbd4947d4bd0f2dcde49058b8@prod.ol.epicgames.com/V2:launcher:WIN::00E4DD674A943E94A35A2DB8D6B0F5E9"><body>{"type":"com.epicgames.party.invitation","payload":{"partyId":"c0169f5a67784d71ab4a0468ceb2c139","partyTypeId":286331153,"displayName":"Jawschamp","accessKey":"k","appId":"Fortnite","buildId":"15555257"},"timestamp":"2021-03-19T18:47:58.317Z"}</body></message>
+    // <message id="2d085420-4db7-4196-b5eb-61c1cd41ef60" from="xmpp-admin@prod.ol.epicgames.com" corr-id="ITS-IHYXmPPLw1cXw6nCyDR_VQ" xmlns="jabber:client" to="9baf4e2cbd4947d4bd0f2dcde49058b8@prod.ol.epicgames.com/V2:launcher:WIN::00E4DD674A943E94A35A2DB8D6B0F5E9"><body>{"type":"com.epicgames.social.interactions.notification.v1","interactions":[{"fromAccountId":"d4d0142cc75141ff841b1b41b40bed3a","toAccountId":"9baf4e2cbd4947d4bd0f2dcde49058b8","interactionType":"PingSent","happenedAt":1616179678140,"app":null,"namespace":"Fortnite"}]}</body></message>
+    // <message id="fc19d21a-4bed-48a9-a407-511c32d19de6" from="xmpp-admin@prod.ol.epicgames.com" corr-id="ITS-YCbVu3ur4Gkm0itmby-ngg" xmlns="jabber:client" to="9baf4e2cbd4947d4bd0f2dcde49058b8@prod.ol.epicgames.com/V2:launcher:WIN::00E4DD674A943E94A35A2DB8D6B0F5E9"><body>{"type":"com.epicgames.social.interactions.notification.v1","interactions":[{"fromAccountId":"d4d0142cc75141ff841b1b41b40bed3a","toAccountId":"9baf4e2cbd4947d4bd0f2dcde49058b8","interactionType":"PartyInviteSent","happenedAt":1616179678140,"app":null,"namespace":"Fortnite"}]}</body></message>
     class XmppClient {
     public:
         XmppClient(const std::string& AccountId, const std::string& AccessToken);
